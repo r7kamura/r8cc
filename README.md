a032860e8d45cab6ceaaaf2c9063d0a5cafc4aae
r7kamura <r7kamura@gmail.com>
2020-04-16 00:53:21 +0900

    Roughly support '+' and '-' and whitespace

--- README.md
+++ README.md
@@ -10,27 +10,29 @@ A toy C compiler written in Rust.
 $ cargo run
     Finished dev [unoptimized + debuginfo] target(s) in 0.04s
      Running `target/debug/r8cc`
-Usage: r8cc <NUMBER>
-$ cargo run -- 123
+Usage: r8cc <PROGRAM>
+$ cargo run -- "1 + 2 - 3"
     Finished dev [unoptimized + debuginfo] target(s) in 0.00s
-     Running `target/debug/r8cc 123`
+     Running `target/debug/r8cc '1 + 2 - 3'`
 .intel_syntax noprefix
 .global main
 main:
-  mov rax, 123
+  mov rax, 1
+  add rax, 2
+  sub rax, 3
   ret
 ```
 
 Assemble output by `cc`:
 
 ```console
-$ cargo run -- 123 > tmp.s
+$ cargo run -- "1 + 2 - 3" > tmp.s
     Finished dev [unoptimized + debuginfo] target(s) in 0.00s
-     Running `target/debug/r8cc 123`
+     Running `target/debug/r8cc '1 + 2 - 3'`
 $ docker-compose run --rm base cc -o tmp tmp.s
 $ docker-compose run --rm base ./tmp
 $ echo $?
-123
+0
 ```
 
 ## Naming
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 fn warn_usage() {
-    eprintln!("Usage: r8cc <NUMBER>");
+    eprintln!("Usage: r8cc <PROGRAM>");
+}
+
+fn consume_number(peekable: &mut std::iter::Peekable<std::str::Chars>) -> u32 {
+    let mut number = 0;
+
+    while let Some(&character) = peekable.peek() {
+        if character.is_ascii_digit() {
+            peekable.next();
+            number = number * 10 + character.to_digit(10).unwrap();
+        } else if character.is_whitespace() {
+            peekable.next();
+        } else {
+            break;
+        }
+    }
+    number
 }
 
 fn main() {
@@ -9,14 +25,31 @@ fn main() {
         return;
     }
 
-    let number = args.nth(1).unwrap().parse::<u32>();
-    if let Ok(number) = number {
-        println!(".intel_syntax noprefix");
-        println!(".global main");
-        println!("main:");
-        println!("  mov rax, {}", number);
-        println!("  ret");
-    } else {
-        warn_usage();
+    let input = args.nth(1).unwrap();
+    let mut chars = input.chars().peekable();
+
+    let number = consume_number(&mut chars);
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("  mov rax, {}", number);
+    while let Some(&character) = chars.peek() {
+        match character {
+            '+' => {
+                chars.next();
+                println!("  add rax, {}", consume_number(&mut chars));
+            },
+            '-' => {
+                chars.next();
+                println!("  sub rax, {}", consume_number(&mut chars));
+            },
+            ' ' => {
+                chars.next();
+            },
+            _ => {
+                panic!("Unexpected token: '{}'", chars.next().unwrap());
+            },
+        }
     }
+    println!("  ret");
 }
